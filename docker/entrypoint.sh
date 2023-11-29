a8237d384360d31f2118d777db6742a9657a691a
NTA Ho Huu Thang <thanghh.nta@gmail.com>
2023-11-29 16:38:07 +0700

    add

---
+++ docker/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install
+fi
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+    case "$APP_ENV" in
+    "local")
+        echo "Copying .env.example ... "
+        cp .env.example .env
+    ;;
+    "prod")
+        echo "Copying .env.prod ... "
+        cp .env.prod .env
+    ;;
+    esac
+else
+    echo "env file exists."
+fi
+
+# php artisan migrate
+php artisan clear
+php artisan optimize:clear
+
+# Fix files ownership.
+chown -R www-data .
+chown -R www-data /app/storage
+chown -R www-data /app/storage/logs
+chown -R www-data /app/storage/framework
+chown -R www-data /app/storage/framework/sessions
+chown -R www-data /app/bootstrap
+chown -R www-data /app/bootstrap/cache
+
+# Set correct permission.
+chmod -R 775 /app/storage
+chmod -R 775 /app/storage/logs
+chmod -R 775 /app/storage/framework
+chmod -R 775 /app/storage/framework/sessions
+chmod -R 775 /app/bootstrap
+chmod -R 775 /app/bootstrap/cache
+
+php-fpm -D
+nginx -g "daemon off;"
